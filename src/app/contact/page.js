6dff5337a08da98edb670139cd27bb0e6f43027a
shaikot07 <saifulislamshaikat007@gmail.com>
2024-04-25 22:56:55 +0600

    explore Multi routing in next.js

---
+++ src/app/contact/page.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const contact = () => {
+    return (
+        <div>
+           <h2 className='text-orange-500 text-4xl text-center'> this is  a contact pages</h2>
+        </div>
+    );
+};
+
+export default contact;
