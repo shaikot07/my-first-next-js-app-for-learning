5def51f69b7ed2939d336f92b425eb359d98e121
shaikot07 <saifulislamshaikat007@gmail.com>
2024-04-25 22:45:00 +0600

    explore routing

---
+++ src/app/about/me/page.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const meAbout = () => {
+    return (
+        <div>
+            <h1 className=''>every thing about me</h1>
+        </div>
+    );
+};
+
+export default meAbout;
---
+++ src/app/about/page.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const aboutPages = () => {
+    return (
+        <div>
+            this is a about pages
+        </div>
+    );
+};
+
+export default aboutPages;
