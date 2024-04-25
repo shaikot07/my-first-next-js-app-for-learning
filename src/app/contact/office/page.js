7bf6c2f7dfdc52e33d250aaae0f8e9f6d91799c8
shaikot07 <saifulislamshaikat007@gmail.com>
2024-04-25 22:59:16 +0600

    explore nested routing system in next.js app

---
+++ src/app/contact/office/page.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const officeInfo = () => {
+    return (
+        <div>
+            this is s office info pages
+        </div>
+    );
+};
+
+export default officeInfo;
