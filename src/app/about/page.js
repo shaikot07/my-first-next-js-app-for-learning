8931ae67f500bbf80e7301d3e465a67b009a5edb
shaikot07 <saifulislamshaikat007@gmail.com>
2024-04-25 22:53:28 +0600

    butify about route

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -3,7 +3,10 @@ import React from 'react';
 const aboutPages = () => {
     return (
         <div>
-            this is a about pages
+           <h2 className='text-yellow-500 text-4xl text-center font-semibold'> This is a about pages</h2>
+           <div>
+            
+           </div>
         </div>
     );
 };
