da30a1cade4788e67114637a3f9643b87bf677cf
shaikot07 <saifulislamshaikat007@gmail.com>
2024-04-25 23:03:43 +0600

    explore nestade route in nestaded > nestaded route

--- src/app/contact/office/page.js
+++ src/app/contact/office/page.js
@@ -3,7 +3,7 @@ import React from 'react';
 const officeInfo = () => {
     return (
         <div>
-            this is s office info pages
+            <h2 className='text-orange-500 text-4xl text-center'> this is  a contact pages <span className='text-green-700 text-6xl underline'>office info pages 😁😆😅😂</span></h2>
         </div>
     );
 };
