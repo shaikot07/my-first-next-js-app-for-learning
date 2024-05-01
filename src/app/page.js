1a963758e44cc186708cd0c9525510a587033708
shaikot07 <saifulislamshaikat007@gmail.com>
2024-05-01 22:35:53 +0600

    install daisyui and added plugins requre in tailwind.config file

--- src/app/page.js
+++ src/app/page.js
@@ -4,6 +4,7 @@ export default function Home() {
   return (
     <div className="max-w-6xl mx-auto h-1/2 mt-5">
       <h1 className="text-4xl text-green-800 font-bold text-center">This is a home pages 😁😆😅😂 </h1>
+      <button className="btn btn-primary">incress</button>
     </div>
   );
 }
