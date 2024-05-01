912aec230cdd9e83ab987edc018c286353dcbe79
shaikot07 <saifulislamshaikat007@gmail.com>
2024-05-01 22:49:01 +0600

    explore more route

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -1,11 +1,15 @@
+import Link from 'next/link';
 import React from 'react';
 
 const aboutPages = () => {
     return (
         <div>
            <h2 className='text-yellow-500 text-4xl text-center font-semibold'> This is a about pages</h2>
-           <div>
-            
+
+           <div className='max-w-6xl mx-auto'>
+            <Link href='/' className='bg-green-500 py-3 px-6 rounded-lg text-white'>
+                Go Back Home 
+            </Link>
            </div>
         </div>
     );
--- src/app/page.js
+++ src/app/page.js
@@ -5,10 +5,13 @@ export default function Home() {
   return (
     <div className="max-w-6xl mx-auto h-1/2 mt-5">
       <h1 className="text-4xl text-green-800 font-bold text-center">This is a home pages 😁😆😅😂 </h1>
-    
+    <h2 className="text-purple-500 font-bold text-center text-2xl">Explore navigate</h2>
+      <div className="max-w-[200px] mx-auto">
       <Link href='/about'>
-        <button className="btn btn-primary">Go to about  pages</button>
+        <button className="btn btn-primary text-center">Go to about  pages</button>
       </Link>
+
+      </div>
     </div>
   );
 }
