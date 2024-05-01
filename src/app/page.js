a0ce05d05ecdacbf95267e26b82e7f2696b7fb51
shaikot07 <saifulislamshaikat007@gmail.com>
2024-05-01 22:40:57 +0600

    exploring navigate using Link

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,14 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <div className="max-w-6xl mx-auto h-1/2 mt-5">
       <h1 className="text-4xl text-green-800 font-bold text-center">This is a home pages 😁😆😅😂 </h1>
-      <button className="btn btn-primary">incress</button>
+    
+      <Link href='/about'>
+        <button className="btn btn-primary">Go to about  pages</button>
+      </Link>
     </div>
   );
 }
