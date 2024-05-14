f7b7fc550bdb44ef334cd8a7bb975e49759395f9
shaikot07 <saifulislamshaikat007@gmail.com>
2024-05-14 16:45:23 +0600

    explore dynamic data fetchin by dynamic id

--- src/app/post/[id]/page.js
+++ src/app/post/[id]/page.js
@@ -1,6 +1,10 @@
 import React from 'react';
 
-const DetailPage = () => {
+const DetailPage = async({params}) => {
+    console.log(params);
+    const res = await fetch(`http://localhost:5000/posts/${params.id}`);
+    const postDetals = await res.json();
+    console.log(postDetals);
     return (
         <div>
             <h2>this is a detail page</h2>
