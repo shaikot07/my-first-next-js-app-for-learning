71937e5c44427768cf7d52118148f6179864ff54
shaikot07 <saifulislamshaikat007@gmail.com>
2024-05-14 21:55:05 +0600

    explore  generateStaticParams

--- src/app/post/[id]/page.js
+++ src/app/post/[id]/page.js
@@ -1,8 +1,25 @@
 import Link from 'next/link';
 import React from 'react';
 
+// next.js ar documention  theke copy kore anechi
+export async function generateStaticParams() {
+    const res = await fetch("http://localhost:5000/posts");
+    const posts = await res.json();
+
+    const ids = posts.map((post)=>{
+        return{
+            id:post.id + "",
+        }
+    })
+    console.log(ids);
+    return ids;
+  }
+  
+
+
+
 const DetailPage = async ({ params }) => {
-    console.log(params);
+    // console.log(params);
     const res = await fetch(`http://localhost:5000/posts/${params.id}`);
     const postDetals = await res.json();
     console.log(postDetals);
