05955648460971fa73f63c707057e4b358cef082
shaikot07 <saifulislamshaikat007@gmail.com>
2024-06-15 23:08:48 +0600

    added new folder and new route

---
+++ src/app/about/newme/page.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const page = () => {
+    return (
+        <div>
+            <h2>this is a new route </h2>
+            <h3>Pages name is a  newMe pages</h3>
+        </div>
+    );
+};
+
+export default page;
