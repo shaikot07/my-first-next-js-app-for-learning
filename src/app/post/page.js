5b18ab5e471457c7083cb234cf2a09cc2ef09381
shaikot07 <saifulislamshaikat007@gmail.com>
2024-11-12 22:01:50 +0600

    change bg-color and text color and button style:

--- src/app/post/page.js
+++ src/app/post/page.js
@@ -9,14 +9,14 @@ const page = async () => {
       <h2 className='text-3xl text-center'>Total post :{posts.length}</h2>
       <div>
         {
-          posts.map((post, ind) => <div key={ind} className="card w-[50%] mx-auto mt-5 bg-primary text-primary-content">
+          posts.map((post, ind) => <div key={ind} className="card w-[50%] mx-auto mt-5 bg-gray-200 text-black">
             <div className="card-body">
               <h2 className="card-title"> title {post.title} </h2>
               <p>Description {post.description}</p>
               <p className='mt-3 font-semibold text-red-700'>Like:  {post.likeCount}</p>
               <div className="card-actions justify-end">
                 <Link href={`/post/${post.id}`}>
-                  <button className="btn">See More</button>
+                  <button className="btn bg-purple-900 text-white hover:bg-black">See More</button>
                 </Link>
               </div>
             </div>
