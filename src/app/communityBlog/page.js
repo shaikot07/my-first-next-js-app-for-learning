94bc8529977662978f9a7ec6005f9671f32b0311
shaikot07 <saifulislamshaikat007@gmail.com>
2024-11-18 23:18:06 +0600

    added new page

---
+++ src/app/communityBlog/page.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const CommunityBlogPage = () => {
+    return (
+        <div>
+            this is a community blog page here 
+        </div>
+    );
+};
+
+export default CommunityBlogPage;
