dc41064bb8ae684938c3b14e13965e5ca8c56e9e
shaikot07 <saifulislamshaikat007@gmail.com>
2024-11-20 21:47:39 +0600

    start making blog card

--- src/app/communityBlog/page.js
+++ src/app/communityBlog/page.js
@@ -1,9 +1,13 @@
+import Image from 'next/image';
 import React from 'react';
 
 const CommunityBlogPage = () => {
     return (
         <div>
-            this is a community blog page here 
+            this is a community blog page here j p ty f 
+            <div>
+                <Image src='' alt='blog image'/>
+            </div>
         </div>
     );
 };
